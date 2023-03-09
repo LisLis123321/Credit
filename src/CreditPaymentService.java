2cc50868f298525f30ff84aefa40fecd3c5dd382
Aleksandr <hitrovaleks2014@gmail.com>
2023-03-09 18:31:30 +0300

    commit+

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    public double calculate(int sumCredit, double percent, int month) {
+    public int calculate(int sumCredit, double percent, int month) {
         double monthlyPayment = sumCredit * ((percent/100/12) + ((percent/100/12) / (Math.pow(1+ (percent/100/12), month) - 1)));
         return (int) monthlyPayment;
     }
